a7c0f42a96ee8f0c92ce9541a2c54a5ca2653394
Makayla <makaylabusher@gmail.com>
2022-12-12 13:49:21 -0600

    add sad/happy path for update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,21 +23,24 @@ class ProductsController < ApplicationController
     if @product.save
       render json: @product.as_json
     else 
-      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
     end
     # render "products/show"
   end
 
   def update
-    @product = Product.find_by(id: params[:id])
+    product = Product.find_by(id: params[:id])
     product.name = params[:name]
     product.price = params[:price]
     product.image_url = params[:image_url]
     product.description = params[:description]
     product.inventory = params[:inventory]
-    product.save
-    # render json: product.as_json
-    render "products/show"
+    if product.save
+      render json: product.as_json
+    # render "products/show"
+    else 
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
