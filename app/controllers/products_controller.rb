a8a3eb73ea91b2fbde0ae98062de442c33ed7818
Makayla <makaylabusher@gmail.com>
2022-12-13 13:00:12 -0600

    fix update action to be more dynamic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,12 +30,12 @@ class ProductsController < ApplicationController
 
   def update
     product = Product.find_by(id: params[:id])
-    product.name = params[:name]
-    product.price = params[:price]
-    product.image_url = params[:image_url]
-    product.description = params[:description]
-    product.inventory = params[:inventory]
-    product.supplier_id = params[:supplier_id]
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.inventory = params[:inventory] || product.inventory
+    product.supplier_id = params[:supplier_id] || product.supplier_id
     if product.save
       render json: product.as_json
     # render "products/show"
