b7ce63cc3bf9551c0ecb178f16bf225067ec5cb6
Makayla <makaylabusher@gmail.com>
2022-12-13 15:03:19 -0600

    fix bugs in index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,6 @@ class ProductsController < ApplicationController
     product = Product.new(
       name: params[:input_name],
       price: params[:input_price],
-      image_url: params[:input_image_url],
       description: params[:input_description],
       inventory: params[:input_inventory]
     )
@@ -32,7 +31,6 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     product.name = params[:name] || product.name
     product.price = params[:price] || product.price
-    product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
     product.inventory = params[:inventory] || product.inventory
     product.supplier_id = params[:supplier_id] || product.supplier_id
