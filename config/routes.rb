0af6d34ba5934221fd02b434891dacee02c13407
Makayla <makaylabusher@gmail.com>
2022-12-14 12:55:18 -0600

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
