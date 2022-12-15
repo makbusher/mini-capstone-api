20b66c4b6381fa326d283f05afe2353438ef663e
Makayla <makaylabusher@gmail.com>
2022-12-15 13:39:21 -0600

    dynamic orders create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,36 @@
 class OrdersController < ApplicationController
   def create 
-    
+
+    product = Product.find_by(id: params[:product_id])
+    subtotal = product.price * params[:quantity]
+
+    tax_rate = 0.09
+    tax = subtotal * tax_rate
+    total = subtotal + tax
+    if current_user
+      order = Order.new(
+      user_id: current_user.id, 
+      product_id: params[:product_id], 
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total
+      )
+      if order.save
+        render json: order.as_json(methods: [:subtotal, :tax, :total])
+      else
+        render json: order.error.full_messages
+      end
+    else
+      render json: {message: "You must login first"}
+    end
+  end
+  def show 
+    order = Order.find_by(id: params[:id])
+    render json: order.as_json
+  end
+  def index 
+    orders = Order.all
+    render json: orders.as_json
+  end
 end
