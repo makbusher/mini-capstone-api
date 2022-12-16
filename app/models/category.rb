8eac44ac5e0d8d84376f9a40a8c6c8af02dfd264
Makayla <makaylabusher@gmail.com>
2022-12-16 14:20:11 -0600

    add has many through associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,4 @@
 class Category < ApplicationRecord
   has_many :category_products
-
-  def products 
-    products = []
-    category_products.each do |category_product|
-      p category_product.product
-    end
-  end
+  has_many :products, through: :category_products
 end
